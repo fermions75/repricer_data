bd1d3e3e6b7afbdb5ea5a8b8e5f2e89afce4a240
Farhan Ishraq Omi <omifarhan@gmail.com>
2024-04-20 11:06:44 +0600

    optimized repricer process

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ import requests
 import os
 
 
-def get_pricing_data():
+
+def write_msg_to_file(msg):
+    with open('logger.txt', 'a') as f:
+        f.write('\n' + msg)
+
+
+def main():
     try:
         url = os.environ['URL']
     except KeyError as e:
@@ -21,57 +27,25 @@ def get_pricing_data():
             "B08R6DMVHH",
             "B0CLZH9RM4",
             "B098QMD9LV"
-        ]
+        ],
+        "last_row_number": process.get_last_row_number()
     }
 
     response = requests.get(url, json=data)
     response_dict = response.json()
     status_code = response.status_code
-    return response_dict['result'], status_code
-
-
-
-
-def main():
-    sheet = process.connect_sheet()
-    worksheet = sheet.get_worksheet(0)
-
-    last_50_rows = process.get_last_50_rows(worksheet)
-
-    price_data, status_code = get_pricing_data()
-    curr_bb_winner = process.get_bb_winner_curr(price_data)
-    recent_data = process.fetch_recent_data(last_50_rows)
-    prev_bb_winner = process.get_bb_winner_prev(recent_data)
-
-
-    price_data = process.add_event_id(price_data, curr_bb_winner, prev_bb_winner)
-
-    # # Prepare the data for the rows
-    rows = [list(row.values()) for row in price_data]
-
-    # Append all rows at once
-    worksheet.append_rows(rows)
-    process.save_row_number(len(rows))
-    print("Data has been written to the Google Sheet")
-
-
-
-def first():
-    sheet = process.connect_sheet()
-    worksheet = sheet.get_worksheet(0)
-
-    price_data, status_code = get_pricing_data()
-    for data in price_data:
-        data['event_id'] = process.generate_event_id(data['fba_seller_id'], data['fba_date'], data['fba_time'])
+    msg = response_dict.get('message')
+    if status_code != 200:
+        print(f"Error: {msg}")
+        write_msg_to_file(f"{datetime.now()} - Error: {msg}")
+    else:
+        print(f"Success: {msg}")
+        write_msg_to_file(f"{datetime.now()} - Success: {msg}")
+        row_number = response_dict.get('rows_written')
+        process.save_row_number(row_number)
     
-    rows = [list(row.values()) for row in price_data]
 
-    # Append all rows at once
-    worksheet.append_rows(rows)
-    process.save_row_number(len(rows))
-    print("Data has been written to the Google Sheet")
 
 
 if __name__ == "__main__":
-    # first()
     main()
